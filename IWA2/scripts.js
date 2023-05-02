d7df10190a6e288797089591a17b1229ba37a634
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-05-02 14:10:12 +0200

    modified incorrectly written js code

---
+++ IWA2/scripts.js
@@ -0,0 +1,19 @@
+/* CHALLENGE 1 */
+
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user */ 
+
+const surname = 'Smith'
+
+
+console.log(user, surname)
+
+// The issue with the callenge was that the comments were not inserted correctly and the console log input was also ivalid
+
+
+// CHALLENGE 2
+
