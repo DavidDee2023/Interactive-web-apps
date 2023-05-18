340c1a0cfa988f5fae7197c15da056f3a5c0b431
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-05-18 14:39:27 +0200

    modified and completed IWA7 Challenge1

--- IWA7/Challenge1/scripts.js
+++ IWA7/Challenge1/scripts.js
@@ -1,2 +1,6 @@
-const value = "3"
+const value = 3
 console.log(value + 4 + value)
+
+// "3" is a string and 3 is anumber
+
+//So the reasom it produced 343 is because a 3 with double quotes is a string and without qoute it will log the sum of the numbers giving 10
