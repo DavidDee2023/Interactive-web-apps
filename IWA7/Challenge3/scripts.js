a66ff6fa743d2f7fe4c9023952df7f7ca3631ffa
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-05-22 08:28:19 +0200

    modified IWA7/Challenge3/scripts.js

---
+++ IWA7/Challenge3/scripts.js
@@ -0,0 +1,20 @@
+const leoName = 'Leo'
+const leoSurname = 'Musvaire     '
+const leoBalance = '-9394'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+
+const divider = '----------------------------------'
+
+// Only change below this line
+
+const owed = parseInt('R' + leoBalance + sarahBalance)
+const leo = "${leoName} + ${leoSurname} + (\"owed:\" + \"R\" + leoBalance )\n"
+const sarah = "${sarahName} + ${sarahSurname} + (\"owed:\" + \"R\" + sarahBalance) \n"
+"\n"
+const total = "\n\Total amount owed:\n"
+const result = leo + sarah + divider + divider + total + owed + divider
+
+console.log(result)
