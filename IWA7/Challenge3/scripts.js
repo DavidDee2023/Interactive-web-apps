e0ece4b4461d4a084790bb85288d40d5d6e7bc1c
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-05-23 17:47:21 +0200

    modified IWA7 and IWA9

--- IWA7/Challenge3/scripts.js
+++ IWA7/Challenge3/scripts.js
@@ -1,8 +1,8 @@
 const leoName = 'Leo'
-const leoSurname = 'Musvaire     '
+const leoSurname = 'Musvaire'
 const leoBalance = '-9394'
 
-const sarahName = 'Sarah    '
+const sarahName = 'Sarah'
 const sarahSurname = 'Kleinhans'
 const sarahBalance = '-4582.21000111'
 
@@ -10,11 +10,12 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = parseInt('R' + leoBalance + sarahBalance)
-const leo = "${leoName} + ${leoSurname} + (\"owed:\" + \"R\" + leoBalance )\n"
-const sarah = "${sarahName} + ${sarahSurname} + (\"owed:\" + \"R\" + sarahBalance) \n"
+const owed = parseInt(leoBalance )+ parseInt(sarahBalance)
+const leo = `${leoName} ${leoSurname} (owed: R ${leoBalance} )\n`
+const sarah = `${sarahName} ${sarahSurname} (owed: R ${sarahBalance}) \n`
 "\n"
-const total = "\n\Total amount owed:\n"
+const total = "\n\Total amount owed: 'R'"
+"\n"
 const result = leo + sarah + divider + divider + total + owed + divider
 
 console.log(result)
--- IWA9/Challenge1/scripts.js
+++ IWA9/Challenge1/scripts.js
@@ -5,20 +5,20 @@ const size = 'large'
 
 // Only change the syntax below (not the values or key names)
 
-const expenses = {
+let expenses = {
     food: 51.7501,
-    transport:  10.2,
-}
+    transport: 10.2,
+};
   
-const tax = {
+let tax = {
     734: '3%',
     234: '20%',
     913: '12%',
     415: '38%',
     502: '42%',
-}
+};
 
-const rent = {
+let rent = {
     none: 0,
     'small-room': 200,
     'large-room': 300,
@@ -26,12 +26,13 @@ const rent = {
     'large-apartment': 800,
     'small-house': 1200,
     'large-house': 2400,
-}
+};
 
 // You can change below however you want
 
-const taxAsDecimal = tax.913 / 100
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
+	const taxAsDecimal = parseInt(tax[913]/ 100)
+	const startingAfterTax = salary * 1 - taxAsDecimal
+	const type = lodging + size
+	const balance = expenses(transport) - expenses(food) - expenses(rent.type)
+
 console.log(balance)
