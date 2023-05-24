358ecc73567bb1eabd4666c7cc22c9f8ad70b581
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-05-24 10:31:31 +0200

    modified IWA7 challeng3

--- IWA7/Challenge3/scripts.js
+++ IWA7/Challenge3/scripts.js
@@ -10,12 +10,16 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = parseInt(leoBalance )+ parseInt(sarahBalance)
-const leo = `${leoName} ${leoSurname} (owed: R ${leoBalance} )\n`
-const sarah = `${sarahName} ${sarahSurname} (owed: R ${sarahBalance}) \n`
-"\n"
-const total = "\n\Total amount owed: 'R'"
-"\n"
-const result = leo + sarah + divider + divider + total + owed + divider
+const owed = parseInt(leoBalance)+ parseInt(sarahBalance)
+const leo = `${leoName} ${leoSurname}(owed: R ${leoBalance})\n`
+const sarah = `${sarahName} ${sarahSurname}(owed: R ${sarahBalance})\n` + "\n"
 
-console.log(result)
+const total = " \n Total amount owed: R"
+const result = leo + sarah + divider + total + owed  + `\n` + divider
+
+
+        
+        
+
+    console.log(result);
+
