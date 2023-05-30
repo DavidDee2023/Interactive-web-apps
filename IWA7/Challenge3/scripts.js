471c205d72b2f8a096f46469a94696e1ec71aaf4
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-05-30 13:53:33 +0200

    modified IWA7 challenge3

--- IWA7/Challenge3/scripts.js
+++ IWA7/Challenge3/scripts.js
@@ -12,7 +12,7 @@ const divider = '----------------------------------'
 
 const owed = parseInt(leoBalance)+ parseInt(sarahBalance)
 const leo = `${leoName} ${leoSurname}(owed: R ${leoBalance * -1})\n`
-const sarah = `${sarahName} ${sarahSurname}(owed: R ${sarahBalance * -1})\n` + "\n"
+const sarah = `${sarahName} ${sarahSurname}(owed: R ${Math.round(sarahBalance) * -1})\n` + "\n"
 
 const total = [(" \n Total amount owed: R")]
 const result = leo + sarah + divider + total + owed  + `\n` + divider
