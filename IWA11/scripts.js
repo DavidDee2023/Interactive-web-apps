72f1212a3f34512ad9fae26032a5acc27762ffae
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-06-06 13:33:40 +0200

    Modified Files on IWA11 and IWA12

--- IWA11/scripts.js
+++ IWA11/scripts.js
@@ -1,34 +1,34 @@
 // script.js
 
-const 1-root = document.querySelector('[data-key="order1"]'),
-const 1-biscuits = document.querySelector('[data-key="biscuits"]'),
-const 1-donuts= document.querySelector('[data-key="donuts"]'),
-const 1-pancakes = document.querySelector('[data-key="pancakes"]'),
-const 1-status = document.querySelector('[data-key="status"]')
+const order1 = document.querySelector('[data-key="order1"]');
+const biscuits = document.querySelector('[data-key="biscuits"]');
+const donuts= document.querySelector('[data-key="donuts"]');
+const pancakes = document.querySelector('[data-key="pancakes"]');
+const status1 = document.querySelector('[data-key="status"]');
 
-const 2-root: document.querySelector('[data-key="order2"]'),
-const 2-biscuits = document.querySelector('[data-key="biscuits"]'),
-const 2-donuts = document.querySelector('[data-key="donuts"]'),
-const 2-pancakes = document.querySelector('[data-key="pancakes"]'),
-const 2-status = document.querySelector("#status")
+const order2 = document.querySelector('[data-key="order2"]');
+const biscuits2 = document.querySelector('[data-key="biscuits2"]');
+const donuts2 = document.querySelector('[data-key="donuts2"]');
+const pancakes2 = document.querySelector('[data-key="pancakes2"]');
+const status2 = document.querySelector('[data-key="status2"]');
 
-const 3-root = document.querySelector('[data-key=order3"]'),
-const 3-biscuits = document.querySelector('[data-key="biscuits"]'),
-const 3-donuts = document.querySelector('[data-key="donuts"]'),
-const 3-pancakes = document.querySelector('[data-key="pancakes"]'),
-const 3-status = document.querySelector('[data-key="status"]')
+const order3 = document.querySelector('[data-key=order3"]');
+const biscuits3 = document.querySelector('[data-key="biscuits3"]');
+const donuts3 = document.querySelector('[data-key="donuts3"]');
+const pancakes3 = document.querySelector('[data-key="pancakes3"]');
+const status3 = document.querySelector('[data-key="status3"]');
 
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
+biscuits = order1.biscuits,
+donuts = order1.donuts,
+pancakes = order1.pancakes,
+status1 = order1.status ? Delivered : Pending
 
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
+biscuits2= order2.biscuits,
+donuts2 = order2.donuts,
+pancakes2 = order2.pancakes,
+status2 = order2.status ? Delivered : Pending
 
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+biscuits3 = order3.biscuits,
+donuts3 = order3.donuts,
+pancakes3 = order3.pancakes,
+status3 = order3.status ? Delivered : Pending
--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -27,37 +27,42 @@ const STATUS_MAP = {
     }
 }
 
-// Edit below line 
+// Edit below line
+/*  console.log(
+  document.querySelector('[id="foo$bar"] .item')
+  );  */
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+const status1 = document.querySelectorAll('.status1.');
+const reserve1 = document.querySelectorAll('.reserve');
+const checkout1 = document.querySelectorAll('.checkout');
+const checkin1 = document.querySelectorAll('.checkin');
 
-checkin.0.color = none
+const status2 = document.querySelectorAll('.status2');
+const reserve2 = document.querySelectorAll('.reserve');
+const checkout2 = document.querySelectorAll('.checkout');
+const checkin2 = document.querySelectorAll('.checkin');
+
+const status3 = document.querySelectorAll('.status3');
+const reserve3 = document.querySelectorAll('.reserve');
+const checkout3 = document.querySelectorAll('.checkout');
+const checkin3 = document.querySelectorAll('.checkin'); 
+
+checkin1.0.color = none
 status.0.style.color = STATUS_MAP.status.color
 reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
 checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
 checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
 
-checkin.1.color = none
+checkin2.1.color = none
 status.1.style.color = STATUS_MAP.status.color
 reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
 checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
 checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
 
-checkin.2.color = none
+checkin3.2.color = none
 status.2.style.color = STATUS_MAP.status.color
 reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
 checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled' 
