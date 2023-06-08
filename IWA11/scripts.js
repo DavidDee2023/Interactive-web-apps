d798939665616a24716566b0e193ab64e92d12d0
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-06-08 09:41:10 +0200

    Modified files on the project

--- IWA11/scripts.js
+++ IWA11/scripts.js
@@ -1,10 +1,10 @@
 // script.js
 
 const order1 = document.querySelector('[data-key="order1"]');
-const biscuits = document.querySelector('[data-key="biscuits"]');
-const donuts= document.querySelector('[data-key="donuts"]');
-const pancakes = document.querySelector('[data-key="pancakes"]');
-const status1 = document.querySelector('[data-key="status"]');
+const biscuits1 = order1.querySelector('.biscuits .count');
+const donuts1= order1.querySelector('.donuts .count');
+const pancakes1 = order1.querySelector('.pancakes .count');
+const status1 = document.querySelector('.status dd');
 
 const order2 = document.querySelector('[data-key="order2"]');
 const biscuits2 = document.querySelector('[data-key="biscuits2"]');
@@ -12,16 +12,16 @@ const donuts2 = document.querySelector('[data-key="donuts2"]');
 const pancakes2 = document.querySelector('[data-key="pancakes2"]');
 const status2 = document.querySelector('[data-key="status2"]');
 
-const order3 = document.querySelector('[data-key=order3"]');
+const order3 = document.querySelector('[data-key= "order3"]');
 const biscuits3 = document.querySelector('[data-key="biscuits3"]');
 const donuts3 = document.querySelector('[data-key="donuts3"]');
 const pancakes3 = document.querySelector('[data-key="pancakes3"]');
 const status3 = document.querySelector('[data-key="status3"]');
 
-biscuits = order1.biscuits,
-donuts = order1.donuts,
-pancakes = order1.pancakes,
-status1 = order1.status ? Delivered : Pending
+biscuits1.textContent = order1.dataset.biscuits;
+donuts1.textContent = order1.dataset.donuts;
+pancakes1.textContent = order1.dataset.pancakes;
+status1 = order1.status == 'true' ? 'Delivered' : 'Pending';
 
 biscuits2= order2.biscuits,
 donuts2 = order2.donuts,
--- IWA5/Challenge1/scripts.js
+++ IWA5/Challenge1/scripts.js
@@ -2,17 +2,17 @@ const FREE_WARNING = 'Free shipping only applies to single customer orders'
 const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
 const NONE_SELECTED = 0
 
-let customers = 1
-let location = 'RSA'
-let currency = null
+let customers = 1;
+let location = 'RSA';
+let currency = null;
 
 if (shipping === 400 && currency === R) {  location = RSA
-}
+};
 
 if (shipping === 600 && currency === $) { location = NAM
 } else { 
 	shipping === 800 && currency === $
-}
+};
 
 const shoes = 300 * 1;
 const toys = 100 * 5;
@@ -29,12 +29,12 @@ if (shoes + batteries + pens + shirts >= 1000 || $60 ) {
 		    shipping === 0 || calcShipping
 		}
 	}
-}
+};
 
 if (shipping === 0 && customers !== 1) { console.log(FREE_WARNING) }
 
 if (location === 'NK' ) { 
 	console.log(BANNED_WARNIN) || console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-}
+};
 
 //Have to do research about the code
